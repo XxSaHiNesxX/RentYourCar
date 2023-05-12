3753c16f3096263b16184b7d3494e228da6c6bac
XxSaHiNesxX <bartekkanzos@gmail.com>
2023-05-13 01:10:26 +0200

    update
    poprawione ścieżki i zapisywanie zdjęć do bazy danych

--- WebSite/wypozycz.php
+++ WebSite/wypozycz.php
@@ -1,5 +1,18 @@
 <?php
-if($_SERVER["REQUEST_METHOD"] == "POST") {
+$servername = "mysql1.ugu.pl";
+$username = "db700630";
+$password = "projektag";
+$dbname = "db700630";
+
+// połączenie z bazą danych
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// sprawdzenie połączenia
+if ($conn->connect_error) {
+    die("Nieudane połączenie: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $marka = $_POST["marka"];
     $koszt = $_POST["koszt"];
     $data = $_POST["data"];
@@ -10,33 +23,25 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $zdjecie_tmp_name = $_FILES["zdjecie"]["tmp_name"];
     $zdjecie_error = $_FILES["zdjecie"]["error"];
 
-    if($zdjecie_error === 0) {
-        $zdjecie_destination = "zdjecia/" . $zdjecie_name;
+    if ($zdjecie_error === 0) {
+        $sciezka = "zdjecia/"; // Ścieżka do katalogu, w którym są zdjęcia
+        $nazwa_pliku = basename($_FILES["zdjecie"]["name"]); // Pobierz oryginalną nazwę pliku
+
+        $zdjecie_destination = $sciezka . $nazwa_pliku;
         move_uploaded_file($zdjecie_tmp_name, $zdjecie_destination);
     }
-
-    $servername = "mysql1.ugu.pl";
-    $username = "db700630";
-    $password = "projektag";
-    $dbname = "db700630";
-
-    // połączenie z bazą danych
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    // sprawdzenie połączenia
-    if($conn->connect_error) {
-        die("Nieudane połączenie: " . $conn->connect_error);
-    }
-
     // zapytanie SQL dodające rekord
     $sql = "INSERT INTO wypozyczenia (marka, zdjecie, koszt, data, kontakt, opis)
-            VALUES ('$marka', '$zdjecie_name', '$koszt', '$data', '$kontakt', '$opis')";
+            VALUES ('$marka', '$zdjecie_destination', '$koszt', '$data', '$kontakt', '$opis')";
 
-    if($conn->query($sql) === TRUE) {
-        echo "Rekord dodany poprawnie";
+    if ($conn->query($sql) === TRUE) {
+        echo "Ogloszenie dodane poprawnie";
         echo "<script>setTimeout(function(){window.location.href='addoferte.html'}, 3000);</script>";
-        exit();
     } else {
-        echo "Błąd dodawania rekordu: " . $conn->error;
+        echo "Blad dodawania ogłoszenia: " . $conn->error;
     }
+} else {
+    // Błąd przesyłania pliku
+    echo "Blad przesylania pliku.";
 }
+?>
